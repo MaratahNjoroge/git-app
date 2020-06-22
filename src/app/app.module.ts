fc4350b21fd1ee5a193c9fbbdcec46a4340cc841
Maratah <maratah7@gmail.com>
2020-06-23 01:42:35 +0300

    import routes, services and modules of files in the app folder to the file to link through the pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { DatePipe } from './date.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component'
+import { ContactComponent } from './components/contact/contact.component';
+
+import { GithubService } from './github.service';
+import { UserService } from './user.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    DatePipe
+   
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  
   ],
-  providers: [],
+  providers: [ GithubService, UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
